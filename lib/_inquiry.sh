3b9d3771a5ecd77a5a05a5f66b8679f143508585
zostoindia <99603269+zostoindia@users.noreply.github.com>
2022-12-20 14:47:53 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -3,7 +3,7 @@
 get_link_git() {
   
   print_banner
-  printf "${WHITE} 💻 Insira o link GITHUB para iniciar a instalação:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Enter the GITHUB link to start the installation:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " link_git
 }
@@ -11,7 +11,7 @@ get_link_git() {
 get_instancia_add() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o nome da instancia a ser configurada (Utilizar Letras minusculas):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Type the name of the instance to be configured (Use lowercase letters):${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " instancia_add
 }
@@ -19,7 +19,7 @@ get_instancia_add() {
 get_frontend_url() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o domínio da interface FRONTEND:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Enter the FRONTEND interface domain:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " frontend_url
 }
@@ -27,7 +27,7 @@ get_frontend_url() {
 get_frontend_port() {
   
   print_banner
-  printf "${WHITE} 💻 Digite a porta do FRONTEND para esta instancia; Ex: 3333 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Enter the FRONTEND port for this instance; Example: 3333${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " frontend_port
 }
@@ -35,7 +35,7 @@ get_frontend_port() {
 get_backend_url() {
   
   print_banner
-  printf "${WHITE} 💻 Digite o domínio do BACKEND:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Enter the BACKEND domain:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_url
 }
@@ -43,7 +43,7 @@ get_backend_url() {
 get_backend_port() {
   
   print_banner
-  printf "${WHITE} 💻 Digite a porta do BACKEND para esta instancia; Ex: 8080 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Enter the BACKEND port for this instance; Example: 8080 ${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_port
 }
@@ -66,10 +66,10 @@ software_update() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao instalador Owen Zap, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Welcome to the WHATSATENDE installer, Select the next action below!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [1] Instalar o OwenZap\n"
-  printf "   [2] Atualizar o OwenZap\n"
+  printf "   [1] Install a new instance\n"
+  printf "   [2] Update a instance\n"
   printf "\n"
   read -p "> " option
 
