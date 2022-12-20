bbe95e1b773c1ca7e883525ed5cc793070c2b2e0
zostoindia <99603269+zostoindia@users.noreply.github.com>
2022-12-20 14:46:44 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -23,7 +23,7 @@ print_banner() {
   printf "\n\n"
 
   printf "${BLUE}";
-  printf "  INSTALADOR - WHATSATENDE & ZAPFILA by ITALLO PENEDO           \n";
+  printf "  INSTALLER - WHATSATENDE   \n";
   
   printf "\n"
  
